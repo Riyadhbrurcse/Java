149046ac027a7f6a66d89df893d12e7f63fef87a
Riyadh <riyadhbrurcse@gmail.com>
2020-03-21 18:52:29 +0600

    new branch

--- Hello.java
+++ Hello.java
@@ -3,6 +3,9 @@ public class HelloJava{
     public static vaid main(String[] args){
 
         System.out.println("Hello World");
+        System.out.println("Hi Riyadh");
     
     }
-}
+    public static vaid add(int x){
+        return x*x;
+    }
